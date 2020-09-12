4baba3bd5e547779aee516ef2d52a2692248df9c
Tyler Ivie <tylerivie@MacBook-Pro.local>
2020-09-12 17:51:10 -0600

    Styled the components and added the Header component. Need to implement the functional component.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Players from "./Players";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/*<header className="App-header">*/}
+      {/*    <h1>Golf Players</h1>*/}
+      {/*    <h4>Tyler Ivie | DGM 3790</h4>*/}
+      {/*</header>*/}
+        <h1>Golf Players</h1>
+        <h4>Tyler Ivie | DGM 3790</h4>
+        <div className="card-container">
+            <Players/>
+        </div>
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+// import golfData from './golf.json';
+
+// const players = golfData.filter((player => player.BirthState));
+
+class Players extends Component {
+    render() {
+        return (
+            <div className="card-container">
+                {
+                    players.map(player => {
+                        return (
+                            <div className="player-card" key={player.PlayerID}>
+                                <img src={player.PhotoUrl}/>
+                                <div className="player-details">
+                                    <h4>{ player.FirstName } { player.LastName }</h4>
+                                    <small>{ player.BirthCity }, { player.BirthState }</small>
+                                    <small className="labels">Birthplace</small>
+                                    <small>{player.College ? player.College : ''}</small>
+                                    <small className="labels">College</small>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        )
+    }
+}
+
+export default Players;
