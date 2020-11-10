0e6b4a5a83a44283b3a9a16176bb70e30e0b2fc6
Tyler Ivie <tylerivie@MacBook-Pro.local>
2020-11-09 21:34:13 -0700

    Styled movie cards and added data to them.

--- src/Components/Movies.js
+++ src/Components/Movies.js
@@ -1,5 +1,7 @@
 import React, {useEffect, useState} from "react";
 import axios from "axios";
+import PropTypes from 'prop-types';
+
 import {
     Card,
     CardMedia,
@@ -7,16 +9,53 @@ import {
     CardContent,
     CardActions,
     Typography,
-    IconButton, Grid, CardActionArea
+    IconButton, Grid, CardActionArea, TextField
 } from "@material-ui/core";
-import {NavLink} from "react-router-dom";
-import MovieImg from "../assets/movie.png";
-import AppleMusicImg from '../assets/apple-music-note.jpg';
 import Button from "@material-ui/core/Button";
-
+import {makeStyles} from "@material-ui/core/styles";
+import Avatar from '@material-ui/core/Avatar';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import ListItemText from '@material-ui/core/ListItemText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Dialog from '@material-ui/core/Dialog';
+import PersonIcon from '@material-ui/icons/Person';
+import AddIcon from '@material-ui/icons/Add';
+import ErrorIcon from "@material-ui/icons/Error";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+        minHeight: 200
+    },
+    details: {
+        display: 'flex',
+        flexDirection: 'column',
+    },
+    content: {
+        flex: '1 0 auto',
+    },
+    cover: {
+        minWidth: 151,
+    },
+    controls: {
+        display: 'flex',
+        alignItems: 'center',
+        paddingLeft: theme.spacing(1),
+        paddingBottom: theme.spacing(1),
+    },
+    playIcon: {
+        height: 38,
+        width: 38,
+    },
+}));
 
 
 const Movies = () => {
+    const classes = useStyles();
+
     const [movieData, setMovieData] = useState([]);
 
     useEffect(() => {
@@ -46,43 +85,38 @@ const Movies = () => {
     }, [])
 
     return (
-        <div>
-            <Grid container spacing={5} justify="center" className="dashboard">
+        <div className="movie-items-container">
+            <Grid container spacing={6} justify="center" className="dashboard">
                 {
                     movieData.map((movie) => (
-                        <Grid item xs={12} md={5} key={movie.trackId}>
-                            <Card>
+                        <Grid item xs={12} md={3} key={movie.trackId}>
+                            <Card className={classes.root}>
+                                <CardMedia
+                                    className={classes.cover}
+                                    image={movie.artworkUrl100}
+                                    title={movie.trackName}
+                                />
                                 <CardActionArea>
-                                    <CardMedia
-                                        component="img"
-                                        alt={movie.trackName}
-                                        height="100"
-                                        image={movie.artworkUrl100}
-                                        title={movie.trackName}
-                                    />
-                                    <CardContent>
-                                        <Typography gutterBottom variant="h5" component="h2">
-                                            {movie.trackName}
-                                        </Typography>
-                                        <Typography variant="body2" color="textSecondary" component="p">
-                                            {movie.longDescription}
-                                        </Typography>
-                                    </CardContent>
+                                    <div className={classes.details}>
+                                        <CardContent className={classes.content}>
+                                            <Typography component="h5" variant="h5">
+                                                {movie.trackName}
+                                            </Typography>
+                                            <Typography variant="subtitle1" color="textSecondary">
+                                                { movie.artistName }
+                                            </Typography>
+                                            <Typography variant="caption" color="textSecondary">
+                                                { movie.shortDescription }
+                                            </Typography>
+                                        </CardContent>
+                                    </div>
                                 </CardActionArea>
-                                <CardActions>
-                                    <Button size="small" color="primary">
-                                        Share
-                                    </Button>
-                                    <Button size="small" color="primary">
-                                        Learn More
-                                    </Button>
-                                </CardActions>
                             </Card>
                         </Grid>
                     ))
                 }
-
             </Grid>
+
         </div>
     )
 }
